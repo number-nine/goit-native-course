337f8f0c4e948fe80f1f6e1be22d90a8b886b5a3
number-nine <sergii.pokhylko@gmail.com>
2023-08-17 17:32:19 +0300

    go home

--- components/Elements/AvatarHolder/AvatarHolder.js
+++ components/Elements/AvatarHolder/AvatarHolder.js
@@ -4,16 +4,9 @@ import { Pressable, Image, View } from "react-native";
 import AvatarHolderStyles from "./AvatarHolder.styles";
 
 export default AvatarHolder = (props) => {
-    return (
-      <View style={[AvatarHolderStyles.wrapper, props.style]}>
-        <Pressable style={AvatarHolderStyles.button}>
-          <Image
-            style={AvatarHolderStyles.logo}
-            source={{
-              uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
-            }}
-          />
-        </Pressable>
-      </View>
-    );
+  return (
+    <View style={[AvatarHolderStyles.wrapper, props.style]}>
+      <Pressable style={AvatarHolderStyles.button}></Pressable>
+    </View>
+  );
 };
